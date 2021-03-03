b4484e3c7bfaeb949122489bd5c883ac0759934d
Jakub Dlouhý <jakub.dlouhy@outlook.com>
2021-03-03 20:24:57 +0100

    Add new test approach

--- src/android/com/hutchind/cordova/plugins/streamingmedia/StreamingMedia.java
+++ src/android/com/hutchind/cordova/plugins/streamingmedia/StreamingMedia.java
@@ -31,66 +31,18 @@ public class StreamingMedia extends CordovaPlugin {
 		this.callbackContext = callbackContext;
 		JSONObject options = null;
 
-		try {
-			options = args.getJSONObject(1);
-		} catch (JSONException e) {
-			// Developer provided no options. Leave options null.
-		}
-
-		if (ACTION_PLAY_AUDIO.equals(action)) {
-			return playAudio(args.getString(0), options);
-		} else if (ACTION_PLAY_VIDEO.equals(action)) {
-			return playVideo(args.getString(0), options);
-		} else {
-			callbackContext.error("streamingMedia." + action + " is not a supported method.");
-			return false;
-		}
-	}
-
-	private boolean playAudio(String url, JSONObject options) {
-		return play(SimpleAudioStream.class, url, options);
-	}
-	private boolean playVideo(String url, JSONObject options) {
-		return play(NewPlayerCast.class, url, options);
-	}
-
-	private boolean play(final Class activityClass, final String url, final JSONObject options) {
-		final CordovaInterface cordovaObj = cordova;
+        final CordovaInterface cordovaObj = cordova;
 		final CordovaPlugin plugin = this;
 
 		cordova.getActivity().runOnUiThread(new Runnable() {
 			public void run() {
-				final Intent streamIntent = new Intent(cordovaObj.getActivity().getApplicationContext(), activityClass);
-				Bundle extras = new Bundle();
-				extras.putString("mediaUrl", url);
-
-				if (options != null) {
-					Iterator<String> optKeys = options.keys();
-					while (optKeys.hasNext()) {
-						try {
-							final String optKey = (String)optKeys.next();
-							if (options.get(optKey).getClass().equals(String.class)) {
-								extras.putString(optKey, (String)options.get(optKey));
-								Log.v(TAG, "Added option: " + optKey + " -> " + String.valueOf(options.get(optKey)));
-							} else if (options.get(optKey).getClass().equals(Boolean.class)) {
-								extras.putBoolean(optKey, (Boolean)options.get(optKey));
-								Log.v(TAG, "Added option: " + optKey + " -> " + String.valueOf(options.get(optKey)));
-							} else if (options.get(optKey).getClass().equals(Integer.class)) {
-								extras.putInt(optKey, (Integer)options.get(optKey));
-								Log.v(TAG, "Added option: " + optKey + " -> " + String.valueOf(options.get(optKey)));
-							}
-
-						} catch (JSONException e) {
-							Log.e(TAG, "JSONException while trying to read options. Skipping option.");
-						}
-					}
-					streamIntent.putExtras(extras);
-				}
+				final Intent streamIntent = new Intent(cordovaObj.getActivity().getApplicationContext(), MainActivity.class);
 
 				cordovaObj.startActivityForResult(plugin, streamIntent, ACTIVITY_CODE_PLAY_MEDIA);
 			}
 		});
-		return true;
+
+        return true;
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
